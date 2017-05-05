bd89259a8b4db9bdbcc68d4688a5576a377df471
Hsin-Cheng Chao <olala7846@gmail.com>
2017-05-05 09:14:10 +0800

    tune std_a_

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -29,7 +29,7 @@ UKF::UKF() {
 
   // TODO(Olala): tune only the process noise, not the measurement noise
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 3;
+  std_a_ = 2;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
   std_yawdd_ = 1;
